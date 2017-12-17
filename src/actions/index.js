7896f365711e68bbd7238bd567e98d7d274a9b41
Leo Gonzalez <leo.lujan.gonzalez@gmail.com>
2017-12-17 14:20:31 -0200

    initial commit

---
+++ src/actions/index.js
@@ -0,0 +1,1 @@
+const API_KEY = 'e3d0ce951c55fcc866503a9fd323b878';
---
+++ src/containers/search-bar.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+
+export default class SearchBar extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      term: ""
+    };
+    this.onInputChange = this.onInputChange.bind(this);
+  }
+
+  onFormSubmit(event){
+    event.preventDefault();
+    // fetch weather data
+  }
+
+  onInputChange(event){
+    this.setState({term: event.target.value});
+  }
+
+  render(){
+    return (
+      <form onSubmit={this.onFormSubmit} className='input-group'>
+        <input
+          placeholder='Give a five day forecast'
+          className='form-control'
+          value={this.state.term}
+          onChange={this.onInputChange}
+        />
+        <span className='input-group-btn'>
+          <button type='submit' className='btn btn-secondary'>
+            Submit
+          </button>
+        </span>
+      </form>
+    );
+  }
+}
