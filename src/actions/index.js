4a808bf7901efa35609649a004152ae220a4343e
Leo Gonzalez <leo.lujan.gonzalez@gmail.com>
2017-12-18 00:37:39 -0200

    Commit #2

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,15 @@
-const API_KEY = 'e3d0ce951c55fcc866503a9fd323b878';
+import axios from 'axios';
+
+const API_KEY = '4e7e44561ad0127ebef035bd0f8381db';
+const ROOT_URL =`http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city){
+  const url = `${ROOT_URL}&q=${city},bra`;
+  const request = axios.get(url);
+  return {
+    type: FETCH_WEATHER,
+    payload: request
+  };
+};
